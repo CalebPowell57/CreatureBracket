bf28fa1ba23be0e9575d51dbff16d5fe8f8d8f06
jordan Flickinger <77014082+JFlickFusion@users.noreply.github.com>
2021-03-12 15:21:41 -0600

    CSS garbage

--- ClientApp/src/app/bracket-generator/match/match.component.ts
+++ ClientApp/src/app/bracket-generator/match/match.component.ts
@@ -1,21 +1,35 @@
-import {
-  Component,
-  OnInit,
-  Input
-} from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'tt-match',
   templateUrl: './match.component.html',
-  styleUrls: ['./match.component.scss']
+  styleUrls: ['./match.component.scss'],
+  animations: [
+    trigger('popOverState', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide', style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('600ms ease-out')),
+    ])
+  ]
 })
 export class MatchComponent implements OnInit {
 
   @Input() matchups: any;
 
   constructor() { }
+  show = false;
 
   ngOnInit() {
   }
 
+  get stateName() {
+    return this.show ? 'show' : 'hide';
+  }
+  toggle() {
+    this.show = !this.show;
+  }
 }
