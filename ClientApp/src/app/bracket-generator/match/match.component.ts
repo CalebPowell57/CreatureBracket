469dd78f92f6a6de47c656a497c0b99037dbe7fd
Caleb Powell <calebpowell57@yahoo.com>
2021-03-12 18:10:47 -0600

    added voting ui

--- ClientApp/src/app/bracket-generator/match/match.component.ts
+++ ClientApp/src/app/bracket-generator/match/match.component.ts
@@ -3,33 +3,11 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 @Component({
   selector: 'tt-match',
   templateUrl: './match.component.html',
-  styleUrls: ['./match.component.scss'],
-  animations: [
-    trigger('popOverState', [
-      state('show', style({
-        opacity: 1
-      })),
-      state('hide', style({
-        opacity: 0
-      })),
-      transition('show => hide', animate('600ms ease-out')),
-    ])
-  ]
+  styleUrls: ['./match.component.scss']
 })
 export class MatchComponent implements OnInit {
 
   @Input() matchups: any;
 
-  constructor() { }
-  show = false;
-
-  ngOnInit() {
-  }
-
-  get stateName() {
-    return this.show ? 'show' : 'hide';
-  }
-  toggle() {
-    this.show = !this.show;
-  }
+  private creatureVotedFor = '';
 }
