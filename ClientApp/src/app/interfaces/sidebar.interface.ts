ab26982ad6c870bd61be8abde1afb2c908e16d71
Caleb Powell <calebpowell57@yahoo.com>
2021-04-21 08:17:49 -0500

    couple of fixes

--- ClientApp/src/app/interfaces/sidebar.interface.ts
+++ ClientApp/src/app/interfaces/sidebar.interface.ts
@@ -3,7 +3,6 @@ export interface ISidebarParams {
   colActive: boolean;
   matchupId: string;
   isColInit: boolean;
-  isGlobal: boolean;
   selectedAnimationClass: string;
   contentColumnCommand: string;
   creatureVotedForClassSelection: string;
